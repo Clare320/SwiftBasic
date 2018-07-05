ace1e2f6e9e014c61640433668a5966e480ed106
Clare <lilingjie1992@gmail.com>
2018-07-05 22:11:30 +0800

    Clare
    flex codable usage

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -146,3 +146,76 @@ decoder.dateDecodingStrategy = .iso8601
 let plan = try! decoder.decode(FlightPlan.self, from: flyPlan)
 print(plan.aircraft.identification)
 print(plan.actualDepartureDate!)
+
+
+/// --- 重写Decodable或者Encodable
+
+let routeData = """
+{
+    "points": ["KSQL", "KWVI"],
+    "KSQL": {
+        "code":"KSQL",
+        "name":"San Carlos Airport"
+    },
+    "KWVI": {
+        "code":"KWVI",
+        "name":"Watsonville Municipal Airport"
+    }
+}
+""".data(using: .utf8)!
+
+struct Route: Decodable {
+    struct Airport: Decodable {
+        var code: String
+        var name: String
+    }
+    
+    var points: [Airport]
+    
+    private struct CodingKeys: CodingKey {
+        var stringValue: String
+        var intValue: Int? {
+            return nil
+        }
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        init?(intValue: Int) {
+            return nil
+        }
+        static let points = CodingKeys(stringValue: "points")!
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let codes = try container.decode([String].self, forKey: .points)
+        let tmpPoints = try codes.map { (code) -> Airport in
+            let key = CodingKeys(stringValue: code)!
+            let airport = try container.decode(Airport.self, forKey: key)
+            return airport
+        }
+        self.points = tmpPoints
+    }
+}
+
+let routes = try decoder.decode(Route.self, from: routeData)
+print(routes.points)
+
+let reportData = """
+{
+"title":"111",
+"body":"hello world",
+"metadata":{
+"code":"1000",
+"style":"xml"
+}
+}
+""".data(using: .utf8)!
+struct Report: Decodable {
+    var title: String
+    var body: String
+    var metadata: [String: String]
+}
+
+let report = try decoder.decode(Report.self, from: reportData)
+print(report.title)
